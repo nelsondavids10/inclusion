644ab9852527f621935e510ff51b533e36a05cc8
Nelson David Santacruz <nelson.santacruz@macroproyectos.com>
2023-11-30 15:40:36 -0500

    Linea Base

---
+++ inclusion-test/src/main/java/com/inclusion/calculadora/inclusiontest/InclusionTestApplication.java
@@ -0,0 +1,13 @@
+package com.inclusion.calculadora.inclusiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclusionTestApplication.class, args);
+	}
+
+}
---
+++ inclusion-test/src/test/java/com/inclusion/calculadora/inclusiontest/InclusionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inclusion.calculadora.inclusiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclusionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
